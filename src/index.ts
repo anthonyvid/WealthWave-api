6d20e1e9e362ad07585f80a6d605dbf4496e4348
anthony <anthony.vidovic@hootsuite.com>
2023-12-02 22:46:27 -0500

    .

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
 
-import router from "./router";
+// import router from "./router";
 import mongoose from "mongoose";
 
 const app = express();
@@ -26,10 +26,11 @@ server.listen(8080, () => {
   console.log("Server running on http://localhost:8080/");
 });
 
-const MONGO_URL = ""; // DB URI
+const MONGO_URL =
+  "mongodb+srv://wealthwave-anthony:nL0uFkdSoVCq06z1@cluster0.ekxlxcm.mongodb.net/?retryWrites=true&w=majority"; // DB URI
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on("error", (error: Error) => console.log(error));
 
-app.use("/", router());
+// app.use("/", router());
